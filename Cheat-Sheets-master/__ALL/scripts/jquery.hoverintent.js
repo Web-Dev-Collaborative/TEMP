51f36bdb86e379098e3818ab5ffeccb9662dc98b
bgoonz <bryan.guner@gmail.com>
2021-06-07 22:58:48 -0400

    update

--- Cheat-Sheets-master/__ALL/scripts/jquery.hoverintent.js
+++ Cheat-Sheets-master/__ALL/scripts/jquery.hoverintent.js
@@ -11,54 +11,57 @@
     var cfg = {
       interval: 100,
       sensitivity: 6,
-      timeout: 0
+      timeout: 0,
     };
     if ( typeof handlerIn === "object" ) {
-      cfg = $.extend( cfg, handlerIn )
+      cfg = $.extend( cfg, handlerIn );
     } else {
       if ( $.isFunction( handlerOut ) ) {
         cfg = $.extend( cfg, {
           over: handlerIn,
           out: handlerOut,
-          selector: selector
-        } )
+          selector: selector,
+        } );
       } else {
         cfg = $.extend( cfg, {
           over: handlerIn,
           out: handlerIn,
-          selector: handlerOut
-        } )
+          selector: handlerOut,
+        } );
       }
     }
     var cX, cY, pX, pY;
     var track = function ( ev ) {
       cX = ev.pageX;
-      cY = ev.pageY
+      cY = ev.pageY;
     };
     var compare = function ( ev, ob ) {
       ob.hoverIntent_t = clearTimeout( ob.hoverIntent_t );
-      if ( Math.sqrt( ( pX - cX ) * ( pX - cX ) + ( pY - cY ) * ( pY - cY ) ) < cfg.sensitivity ) {
+      if (
+        Math.sqrt( ( pX - cX ) * ( pX - cX ) + ( pY - cY ) * ( pY - cY ) ) <
+        cfg.sensitivity
+      ) {
         $( ob ).off( "mousemove.hoverIntent", track );
         ob.hoverIntent_s = true;
-        return cfg.over.apply( ob, [ ev ] )
+        return cfg.over.apply( ob, [ ev ] );
       } else {
         pX = cX;
         pY = cY;
         ob.hoverIntent_t = setTimeout( function () {
-          compare( ev, ob )
-        }, cfg.interval )
+          compare( ev, ob );
+        }, cfg.interval );
       }
     };
     var delay = function ( ev, ob ) {
       ob.hoverIntent_t = clearTimeout( ob.hoverIntent_t );
       ob.hoverIntent_s = false;
-      return cfg.out.apply( ob, [ ev ] )
+      return cfg.out.apply( ob, [ ev ] );
     };
     var handleHover = function ( e ) {
       var ev = $.extend( {}, e );
       var ob = this;
       if ( ob.hoverIntent_t ) {
-        ob.hoverIntent_t = clearTimeout( ob.hoverIntent_t )
+        ob.hoverIntent_t = clearTimeout( ob.hoverIntent_t );
       }
       if ( e.type === "mouseenter" ) {
         pX = ev.pageX;
@@ -66,21 +69,23 @@
         $( ob ).on( "mousemove.hoverIntent", track );
         if ( !ob.hoverIntent_s ) {
           ob.hoverIntent_t = setTimeout( function () {
-            compare( ev, ob )
-          }, cfg.interval )
+            compare( ev, ob );
+          }, cfg.interval );
         }
       } else {
         $( ob ).off( "mousemove.hoverIntent", track );
         if ( ob.hoverIntent_s ) {
           ob.hoverIntent_t = setTimeout( function () {
-            delay( ev, ob )
-          }, cfg.timeout )
+            delay( ev, ob );
+          }, cfg.timeout );
         }
       }
     };
     return this.on( {
-      "mouseenter.hoverIntent": handleHover,
-      "mouseleave.hoverIntent": handleHover
-    }, cfg.selector )
-  }
+        "mouseenter.hoverIntent": handleHover,
+        "mouseleave.hoverIntent": handleHover,
+      },
+      cfg.selector
+    );
+  };
 } )( jQuery );
